88ed5835ac73b6a08bf7ed458e7d777deeacfe78
Armel Steve OUETCHOUA <armel.ouetchoua@tilkal.com>
2023-04-19 16:39:54 +0200

    add cash capsul component

--- frontend/src/Component/capsules/Capsules.tsx
+++ frontend/src/Component/capsules/Capsules.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react"
 import axios from "axios"
 import './Style.css'
+
 interface CapsuleProps {
   serial: string,
-  type: String,
+  type: string,
   status: string,
 }
 
@@ -12,41 +13,69 @@ const Capsule = () => {
     error = "404"
     return { error }
   }
+
+  const [showTable, setShowTable] = useState(false)
   const [capsulesData, setCapsulesData] = useState<CapsuleProps[]>([]);
 
   useEffect(() => {
-    axios
-      .get("https://api.spacexdata.com/v4/capsules")
-      .then((response) => {
-        setCapsulesData(response.data);
-      })
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://api.spacexdata.com/v4/capsules")
+        setCapsulesData(response.data)
+        if ('caches' in window) {
+          const cache = await caches.open('capsules')
+          await cache.put('https://api.spacexdata.com/v4/capsules', new Response(JSON.stringify(response.data)))
+        }
+      } catch (error) {
+        setError(error.message)
+      }
+    }
 
-      .catch((error) => {
-        setError(error.message);
+    if ('caches' in window) {
+      caches.match('https://api.spacexdata.com/v4/capsules').then((response) => {
+        if (response) {
+          response.json().then((data) => {
+            setCapsulesData(data)
+          })
+        } else {
+          fetchData()
+        }
       })
+    } else {
+      fetchData()
+    }
   }, [])
+
+  const handleButtonClick = () => {
+    setShowTable(true)
+  }
+
   return (
     <>
-    <table>
-        <caption> <h1># Capsules</h1></caption>
-      <thead>
-        <tr>
-          <th>Serial</th>
-          <th>Status</th>
-          <th>Type</th>
-        </tr>
-      </thead>
-      <tbody>
-        {capsulesData.map((capsule, index) => (
-          <tr key={index}>
-            <td>{capsule.serial}</td>
-            <td>{capsule.status}</td>
-            <td>{capsule.type}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-    </> 
+    <caption><button onClick={handleButtonClick}> <h1>Show Capsules</h1></button> </caption>
+      {showTable && ( 
+        <table>
+          <caption> <h1># Capsules</h1></caption>
+          <thead>
+            <tr>
+              <th>Serial</th>
+              <th>Status</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {capsulesData.map((capsule, index) => (
+              <tr key={index}>
+                <td>{capsule.serial}</td>
+                <td>{capsule.status}</td>
+                <td>{capsule.type}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
   )
 }
+
 export default Capsule
