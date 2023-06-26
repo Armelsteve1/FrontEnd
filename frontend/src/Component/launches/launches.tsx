2e9d5a3a50dfe455285c7ca076719241f0139e49
Armel Steve OUETCHOUA <armel.ouetchoua@tilkal.com>
2023-06-26 10:09:40 +0200

    feat() add buttons to components

--- frontend/src/Component/launches/launches.tsx
+++ frontend/src/Component/launches/launches.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from "react"
-import axios from 'axios'
-import './style.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./style.css";
+
 interface LaunchesProps {
-    name: string,
-    details: string,
-    ships: [],
+  name: string;
+  details: string;
+  ships: [];
 }
 
 const Launches = () => {
   const setError = (error: string) => {
-    error = "404"
-    return { error }
-  }
+    error = "404";
+    return { error };
+  };
 
-  const [showTable, setShowTable] = useState(false)
+  const [showTable, setShowTable] = useState(false);
   const [launchesData, setLaunchesData] = useState<LaunchesProps[]>([]);
 
   useEffect(() => {
@@ -22,40 +23,47 @@ const Launches = () => {
       .then((response) => {
         setLaunchesData(response.data);
       })
-
       .catch((error) => {
         setError(error.message);
-      })
-  }, [])
+      });
+  }, []);
 
   const handleButtonClick = () => {
-    setShowTable(true)
-  }
+    setShowTable(!showTable);
+  };
+
   return (
-  <div>
-  
-   <caption><button onClick={handleButtonClick}> <h1>Show launches</h1></button> </caption>
-      {showTable && ( 
-      <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Details</th>
-          <th>Ships</th>
-        </tr>
-      </thead>
-      <tbody>
-        {launchesData.map((launches, index) => (
-          <tr key={index}>
-            <td>{launches.name}</td>
-            <td>{launches.details}</td>
-            <td>{launches.ships.length}</td>
-          </tr>
-          ))}
-      </tbody>
-      </table>
-    )}
-    </div> 
-  )
-}
-export default Launches
+    <div>
+      <caption id="ContentButon">
+        <button onClick={handleButtonClick}>
+          <h1>{showTable ? "Hide launches" : "Show launches"}</h1>
+        </button>
+      </caption>
+      {showTable && (
+        <table>
+          <thead>
+            <caption>
+              <h1># Launches</h1>
+            </caption>
+            <tr>
+              <th>Name</th>
+              <th>Details</th>
+              <th>Ships</th>
+            </tr>
+          </thead>
+          <tbody>
+            {launchesData.map((launches, index) => (
+              <tr key={index}>
+                <td>{launches.name}</td>
+                <td>{launches.details}</td>
+                <td>{launches.ships.length}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default Launches;
